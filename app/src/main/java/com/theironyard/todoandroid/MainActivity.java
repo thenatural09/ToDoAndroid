5fcd833132421eba05405e24f2dc38a7a5750528
oakes <zsoakes@gmail.com>
2016-09-28 10:31:32 -0400

    Start code

--- app/src/main/java/com/theironyard/todoandroid/MainActivity.java
+++ app/src/main/java/com/theironyard/todoandroid/MainActivity.java
@@ -2,12 +2,28 @@ package com.theironyard.todoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView list;
+    EditText text;
+    Button addButton;
+
+    ArrayAdapter<ToDoItem> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.list);
+        text = (EditText) findViewById(R.id.editText);
+        addButton = (Button) findViewById(R.id.button);
+
+        items = new ArrayAdapter<ToDoItem>(this, android.R.layout.simple_list_item_1);
+        list.setAdapter(items);
     }
 }
---
+++ app/src/main/java/com/theironyard/todoandroid/ToDoItem.java
@@ -0,0 +1,15 @@
+package com.theironyard.todoandroid;
+
+/**
+ * Created by zach on 9/28/16.
+ */
+
+public class ToDoItem {
+    String text;
+    boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+}
